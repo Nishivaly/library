1a5cf5c0a5e172451565850da03fe877b9106e97
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-25 20:06:20 -0300

    Initial setup

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function () {
+    const readStatus = this.read === true ? 'already read' : 'not read yet';
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`
+}
+
+
+
+// const book = new Book('HP', "Rowling", 300, false);
+// console.log(book.info());
