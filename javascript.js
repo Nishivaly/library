47eb691a1414feaf60b30d5f58b558fb97c88bce
Nishivaly <51959902+Nishivaly@users.noreply.github.com>
2025-01-25 20:35:59 -0300

    Add logic for displaying books on page

--- javascript.js
+++ javascript.js
@@ -12,7 +12,22 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`
 }
 
+function addBookToLibrary(...args) {
+    const book = new Book(...args);
+    myLibrary.push(book);
+}
 
+addBookToLibrary('HP', "Rowling", 300, false);
+addBookToLibrary('SP', "Rowling", 300, false);
+addBookToLibrary('TWD', "Rowling", 300, false);
 
+const library = document.querySelector('.library');
+
+myLibrary.forEach(book => {
+    const item = document.createElement('p');
+    item.className = 'book';
+    item.textContent = book.info();
+    library.appendChild(item);
+});
 // const book = new Book('HP', "Rowling", 300, false);
 // console.log(book.info());
